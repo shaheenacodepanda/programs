efd126cf45028a0ecd5e254cddec7430ae59b653
shaheenacodepanda <95137817+shaheenacodepanda@users.noreply.github.com>
2021-12-07 13:35:28 +0530

    Add files via upload

---
+++ commonfactors.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+
+{
+	int i;
+	int m=12,n=24;
+	if(m<n)
+		{k=m;
+		}
+	elif(n<m)
+		{k=n;
+		}
+	else 
+		k=n;
+
+	for(i=1;i<=k;i++)
+		{if(m%i)==0 &(n%i==0))
+			print(i,\t);
+		}
+	
+	return 0;
+}
