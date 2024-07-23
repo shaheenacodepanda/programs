223b72b0291ef7806f05f21e895fe802d7647b5b
shaheenacodepanda <95137817+shaheenacodepanda@users.noreply.github.com>
2024-07-23 12:54:29 +0530

    Update assign6_1_bit1set.c

--- assign6_1_bit1set.c
+++ assign6_1_bit1set.c
@@ -13,6 +13,7 @@ union bit_m u={0};
 volatile union bit_m *ptr = (union bit_m*)&u;
 
 ptr->a = 0x2000;
-printf("Resulting value: 0x%08x\n", ptr->a |= 1<<1);
+ptr->a |= 1<<1 | 1<<31;
+printf("Resulting value: 0x%08x\n", ptr->a);
     return 0;
 }
