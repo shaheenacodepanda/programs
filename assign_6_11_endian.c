0c893a33174c8a791f76a75ddeb4e353da7ef3dd
shaheenacodepanda <95137817+shaheenacodepanda@users.noreply.github.com>
2024-07-24 14:24:19 +0530

    Update assign_6_11_endian.c

--- assign_6_11_endian.c
+++ assign_6_11_endian.c
@@ -1,19 +1,17 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 
-void endian_check(int a)
-{   printf("Checking Endian of the processor through 0x%X\n\n", a);
-   
-     int ch = (char)a;
-    if(ch == 0x34)
-    {    printf("Processor is Little Endian:  Since first byte of  0x%X that is 0x%X  is located at Lower Address\n", a, ch);
-    }
-    else{
-         printf("Processor is Big Endian : Since first byte of  0x%X that is 0x%X  is located at Higher Address\n", a, ch);
-    }
-}
 int main() {
-    int a=0x1234;
-    endian_check(a);
+    unsigned int value = 0x231;  // Initial value
+
+    // Extract the lowest byte of the integer using bitwise operations
+    unsigned char byte0 = value & 0xFF;
+
+    // Check if the lowest byte is 0x31 to determine endianness
+    if (byte0 == 0x31) {
+        printf("Processor is Little Endian.\n");
+    } else {
+        printf("Processor is Big Endian.\n");
+    }
+
     return 0;
 }
