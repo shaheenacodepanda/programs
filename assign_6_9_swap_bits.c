06b1e66c796891c2242abbe48e52e8cf35e5a76c
shaheenacodepanda <95137817+shaheenacodepanda@users.noreply.github.com>
2024-07-24 11:55:18 +0530

    Create assign_6_9_swap_bits.c

---
+++ assign_6_9_swap_bits.c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+void print_bits(int a)
+{
+    for( int i=31 ; i >=0; i--)
+    {
+        printf("%d", a>>i&1);
+    }
+    printf("\n");
+}
+void swap_bits(int a)
+{   printf(" Before swapping 0x%x\n", a);
+    print_bits(a);
+    a = (a & 0x55555555)<<1 | (a & 0xAAAAAAAA)>>1;
+    printf(" After swapping 0x%x\n", a);
+    print_bits(a);
+}
+int main() {
+    int a=0x55555555;
+    swap_bits(a);
+    return 0;
+}
