352ecd513e72caea7351fc1e65f28769cf9f11e6
shaheenacodepanda <95137817+shaheenacodepanda@users.noreply.github.com>
2024-07-24 11:56:45 +0530

    Update assign_6_9_swap_bits.c

--- assign_6_9_swap_bits.c
+++ assign_6_9_swap_bits.c
@@ -10,7 +10,9 @@ void print_bits(int a)
     printf("\n");
 }
 void swap_bits(int a)
-{   printf(" Before swapping 0x%x\n", a);
+{   
+    printf(" Swapping EVEN and ODD bits");
+    printf(" Before swapping 0x%x\n", a);
     print_bits(a);
     a = (a & 0x55555555)<<1 | (a & 0xAAAAAAAA)>>1;
     printf(" After swapping 0x%x\n", a);
